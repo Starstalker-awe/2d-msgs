ed902eb74411e1b031bd726ebe48cd722e0ab1a4
Trevor Yates <travelingtrevor123@gmail.com>
2022-11-02 14:22:04 -0400

    Moved login and register to socket routes, responses have yet to be handled by client, created self-signed certificate and began self-hosting socket.io script

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, render_template as render, request, session, redirect, url_for as url
 from datetime import timedelta, datetime
-from Crypto.Cipher import PKCS1_OAEP
 import flask_socketio as socketio
-from Crypto.PublicKey import RSA
 from passlib.hash import argon2
-from Crypto.Hash import SHA256
 from dotmap import DotMap
 import flask_session
 import functools
 import threading
 import tempfile
-import base64
 import uuid
 import cs50
-import os
 
 app = Flask(__name__)
 
-ADMIN_SNOOPING = True
+ADMIN_SNOOPING, DEBUG, SECURE = True, True, False
 
 app.config.update({
     "TEMPLATES_AUTO_RELOAD": True,
@@ -34,7 +29,7 @@ socket_ = socketio.SocketIO(app, async_mode="eventlet")
 DB = cs50.SQL("sqlite:///data.db")
 OVERWATCHERS = DB.execute("SELECT u_id FROM users WHERE overwatcher = 1") if ADMIN_SNOOPING else [] # Admins can see all messages...?
 AUTHORIZED = {room: {id1: False, id2: False} for room, id1, id2 in [(a, *b.split(" ")) for a, b in DB.execute("SELECT * FROM namespaces")]}
-CIPHER = PKCS1_OAEP.new(RSA.importKey(open("./private.pem", 'rb').read()), hashAlgo=SHA256)
+HASH_SETTINGS = {'rounds': 128, 'digest_size': 41, 'salt_size': 8}
 
 
 def login_required(f): # Wrapper for Flask routes
@@ -55,21 +50,19 @@ def index():
 # ==== User Handling Routes ====
 @app.route("/login", methods = ['GET', 'POST']) # Handle both methods
 def login():
-	if request.method == 'POST': # Submitting form
-		form = DotMap(request.form.to_dict()) # Convert to dictionary instead of obscure object
-		if len(user := DB.execute("SELECT * FROM users WHERE username = :un OR email = :un", un = form.username)) == 1 and argon2.verify(form.password, user[0]['password']): # Verify credentials
+	@socket_.on("login", namespace="/login")
+	def login(data):
+		if len(user := DB.execute("SELECT * FROM users WHERE username = :un OR email = :un", un = (form := DotMap(data)).username)) == 1 and argon2.verify(form.password, user[0]['password']): # Verify credentials
 			session.update({'u_id': user[0]['u_id'], 'p_id': user[0]['p_id'], "loggedin": datetime.now().timestamp()}) # Update user session
-			return redirect(url("index")) # Return to index
-		return render("login.html", error = True) # Invalid login credentials
+			socket_.emit("login", {"data": {"error": None, "u_id": user[0]['u_id']}}, namespace="/login", to=request.sid)
+		socket_.emit("login", {"data": {"error": True}}, namespace="/login", to=request.sid)
 	return render("login.html") # Render page
 
 @app.route("/register", methods = ['GET', 'POST']) # Handle get and post
 def register():
 	@socket_.on("register", namespace="/register")
 	def register(data):
-		print(data)
-		form = DotMap(CIPHER.decrypt(base64.b64decode(data)))
-		if all(k in form.keys() for k in ['username', 'password', 'confirm']):
+		if all(k in (form := DotMap(data)).keys() for k in ['username', 'password', 'confirm']):
 			if form.password != form.confirm: socket_.emit("register", {"data": {"error": 1}}, namespace="/register", to=request.sid)
 			if len(DB.execute("SELECT * FROM users WHERE username = :un OR email = :un", un = form.username)) == 0: # Make sure username isn't taken
 				udata = {
@@ -80,9 +73,9 @@ def register():
 				} # Create dict of user's data
 				DB.execute("INSERT INTO users (u_id, username, email, p_id) VALUES (:u_id, :username, :email, :p_id)", **udata)
 				user = DotMap(DB.execute("SELECT * FROM users WHERE u_id = ?", udata['u_id'])[0])
-				threading.Thread(target=lambda:DB.execute("UPDATE users SET password = :pw WHERE u_id = :id", pw = argon2.using(rounds=128,digest_size=41,salt_size=8).hash(form.password), id = user.u_id)).start()
+				threading.Thread(target=lambda:DB.execute("UPDATE users SET password = :pw WHERE u_id = :id", pw = argon2.using(**HASH_SETTINGS).hash(form.password), id = user.u_id)).start()
 				session.update({"id": user.id, "p_id": user.p_id, "loggedin": datetime.now().timestamp()})
-				socket_.emit("register", {"data": {"error": None, "u_id": user.u_id}}) # Return "completed"
+				socket_.emit("register", {"data": {"error": None, "u_id": user.u_id}}, namespace="/register", to=request.sid) # Return "completed"
 			socket_.emit("register", {"data": {"error": 2}}, namespace="/register", to=request.sid) # Username taken
 		socket_.emit("register", {"data": {"error": 3}}, namespace="/register", to=request.sid) # Not all fields supplied
 	return render("register.html") # Render registration page
@@ -115,4 +108,4 @@ def conversation(space):
 
 # ==== Run Server ====
 if __name__ == "__main__":
-	socket_.run(app, debug=True)
+	socket_.run(app, debug=DEBUG, **({"keyfile": "key.pem", "certfile": "cert.pem"} if SECURE else {}))
